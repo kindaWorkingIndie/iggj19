8b78fcffd23335e91ecf2fef495bfeabc81a3d35
Sashify <sasch2010@live.de>
2019-01-25 21:20:31 +0100

    secondary setup

---
+++ iggj19-game/Assets/Scripts/GlobalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour
+{
+
+    [Header("Player Variables")]
+    public float playerSpeed = 5f;
+
+    public InputModel playerOneModel, playerTwoModel;
+
+
+
+    public static GlobalController instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+
+    
+    
+}
---
+++ iggj19-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerNumber { ONE,TWO}
+    public PlayerNumber playerNumber;
+    public GameObject model;
+
+    float moveSpeed;
+
+    public InputModel myInputModel;
+    
+
+    void Start()
+    {
+        if(playerNumber == PlayerNumber.ONE)
+        {
+            myInputModel = GlobalController.instance.playerOneModel;
+        }else if(playerNumber == PlayerNumber.TWO)
+        {
+            myInputModel = GlobalController.instance.playerTwoModel;
+        }
+
+        moveSpeed = GlobalController.instance.playerSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
+
+[System.Serializable]
+public class InputModel
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode knock;
+
+
+    public void SetUpKey(KeyCode key)
+    {
+        this.up = key;
+    }
+    public void SetDownKey(KeyCode key)
+    {
+        this.down = key;
+    }
+    public void SetKnockKey(KeyCode key)
+    {
+        this.knock = key;
+    }
+}
